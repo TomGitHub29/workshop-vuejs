191842d0beb3222c8dde1d99b42d79ba1c20a616
Vivone Tom <tom.vivone@he-arc.ch>
2024-10-31 09:24:34 +0100

    Finish toDo 4

---
+++ main.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data: function() {
+        return {
+            title : 'Achat de café Nespresso',
+            message : 'Achetez votre café en ligne',
+            inStock : true,
+            stock : 10,
+            onSale : false,
+        };
+    },
+
+});
